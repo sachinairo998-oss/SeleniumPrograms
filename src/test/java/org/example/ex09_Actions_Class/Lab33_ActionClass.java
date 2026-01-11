76436a51b5d8d0115493316ff81fe9971965740c
Sachin <sachinairo998@gmail.com>
2026-01-11 10:05:25 +0530

    11 Jan 2026

---
+++ src/test/java/org/example/ex09_Actions_Class/Lab33_ActionClass.java
@@ -0,0 +1,28 @@
+package org.example.ex09_Actions_Class;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Lab33_ActionClass {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        String URL = "https://www.spicejet.com/";
+        driver.get(URL);
+        driver.manage().window().maximize();
+
+        //div[text()='From']/following-sibling::div
+        WebElement origin = driver.findElement(By.xpath("//div[@data-testid=\"to-testID-origin\"]/div/div/input"));
+        WebElement destination = driver.findElement(By.xpath("//div[@data-testid=\"to-testID-destination\"]/div/div/input"));
+
+        Actions actions = new Actions(driver);
+
+
+        actions.moveToElement(origin)
+                .click().sendKeys("BLR").moveToElement(destination).sendKeys("DEL").build().perform();
+    }
+}
